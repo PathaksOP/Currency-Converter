2a873050e87fcafe1e51bfc29f22e3baf74e297a
PathaksOP <namitapathak1983@gmail.com>
2026-05-03 00:15:36 +0530

    added JS in options of currency

---
+++ script.js
@@ -0,0 +1,21 @@
+const base_url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json`;
+
+const dropdown = document.querySelectorAll(".dropdown select");
+
+// for (code in countryList) {
+//   console.log(code , countryList[code]);
+// }
+
+for (let select of dropdown) {
+  for (let currencyCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.value = currencyCode;
+    newOption.innerHTML = currencyCode;
+    select.append(newOption);
+    if (select.id === "from" && currencyCode === "USD") {
+      newOption.selected = true;
+    } else if (select.id === "to" && currencyCode === "INR") {
+      newOption.selected = true;
+    }
+  }
+}
